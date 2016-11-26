bfe54f923e4eede5a35dff48f1db64cceb52efdc
Dmitrii Podlesnykh <buffol.dp@gmail.com>
2016-11-26 16:23:26 +0300

    success simple link with db

--- src/main/java/com/vsu/amm/medframe/app/MainConfiguration.java
+++ src/main/java/com/vsu/amm/medframe/app/MainConfiguration.java
@@ -2,9 +2,13 @@ package com.vsu.amm.medframe.app;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @EnableAutoConfiguration
 @ComponentScan("com.vsu.amm.medframe")
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class MainConfiguration {
 }
--- src/main/java/com/vsu/amm/medframe/entity/User.java
+++ src/main/java/com/vsu/amm/medframe/entity/User.java
@@ -1,7 +1,16 @@
 package com.vsu.amm.medframe.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String firstName;
