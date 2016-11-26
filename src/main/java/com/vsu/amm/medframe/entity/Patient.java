97b56943f0f376656f90b128a457e09688e65c0d
Dmitrii Podlesnykh <buffol.dp@gmail.com>
2016-11-26 15:11:10 +0300

    added sql scripts for user and patient tables and entity fields

--- src/main/java/com/vsu/amm/medframe/entity/Patient.java
+++ src/main/java/com/vsu/amm/medframe/entity/Patient.java
@@ -1,5 +1,104 @@
 package com.vsu.amm.medframe.entity;
 
 
+import java.sql.Date;
+
 public class Patient {
+
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String middleName;
+
+    private Date birthday;
+
+    private String sex;
+
+    private long doctorId;
+
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public Date getBirthdate() {
+        return birthday;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthday = birthdate;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Patient() {
+    }
+
+    public Patient(long id, String firstName,
+                   String lastName, String middleName,
+                   Date birthday, String sex,
+                   long doctorId, String description) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.birthday = birthday;
+        this.sex = sex;
+        this.doctorId = doctorId;
+        this.description = description;
+    }
 }
