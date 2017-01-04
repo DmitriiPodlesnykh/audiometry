5b328135f6bf7c8bf7d445b510e50870e0a9637a
= <buffol.dp@gmail.com>
2017-01-04 03:57:51 +0300

    simple connected controller to .ftl

--- src/main/java/com/vsu/amm/medframe/controller/UserController.java
+++ src/main/java/com/vsu/amm/medframe/controller/UserController.java
@@ -3,6 +3,7 @@ package com.vsu.amm.medframe.controller;
 import com.vsu.amm.medframe.entity.User;
 import com.vsu.amm.medframe.service.Impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
@@ -24,8 +25,10 @@ public class UserController {
         return "redirect:/users";
     }
 
-    @RequestMapping(value = "", method = GET)
-    public String getAll(ModelMap modelMap) throws Exception{
+    @RequestMapping(value = "allusers", method = GET)
+    public String getAll(ModelMap modelMap){
+        modelMap.addAttribute("users", userService.getAll());
         return "user/userList";
     }
+    
 }
--- src/main/resources/templates/user/userList.ftl
+++ src/main/resources/templates/user/userList.ftl
@@ -1,10 +1,15 @@
-<div <!--class="row"-->>
+<div>
     <div>
         <table>
             <tr>
                 <th>First Name</th>
                 <th>Last Name</th>
             </tr>
+        <#list users as user>
+            <tr>
+                <td>${user.id!}</td>
+            </tr>
+        </#list>
         </table>
     </div>
 </div>
