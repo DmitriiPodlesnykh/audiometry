8381b4980dea1743ef1d662e603768815b5b5a9b
= <buffol.dp@gmail.com>
2017-01-11 22:03:21 +0300

    added imports

--- src/main/java/com/vsu/amm/medframe/repository/TemplateRepository.java
+++ src/main/java/com/vsu/amm/medframe/repository/TemplateRepository.java
@@ -2,7 +2,11 @@ package com.vsu.amm.medframe.repository;
 
 import com.vsu.amm.medframe.entity.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TemplateRepository extends JpaRepository<Template, Long>{
